e2616c51ca604e4daf83f244399abcc516076679
boxfish <boxfish893@gmail.com>
2024-03-01 01:34:19 +0900

    feat: エンドポイントの変更とch_idでのクエリ機能の実装

--- src/videos/get.ts
+++ src/videos/get.ts
@@ -6,7 +6,18 @@ type Bindings = {
 
 const chlistGet = new Hono<{ Bindings: Bindings }>();
 
-chlistGet.get("/get", async (c) => {
+chlistGet.get("/", async (c) => {
+  const ch_id = c.req.query("ch_id");
+  if (ch_id) {
+    try {
+      let { results } = await c.env.DB.prepare(
+        `SELECT * FROM videos WHERE ch_id = '${ch_id}'`
+      ).all();
+      return c.json({ result: results });
+    } catch (e) {
+      return c.json({ error: e }, 500);
+    }
+  }
   try {
     let { results } = await c.env.DB.prepare("SELECT * FROM videos").all();
     return c.json({ result: results });
