c9564aaa1ba9dc568486a05af853ee3b8d97c1ad
boxfish <boxfish893@gmail.com>
2024-03-01 12:20:55 +0900

    feat: videosにurlでの検索と話数での検索を実装

--- src/videos/get.ts
+++ src/videos/get.ts
@@ -8,7 +8,19 @@ const chlistGet = new Hono<{ Bindings: Bindings }>();
 
 chlistGet.get("/", async (c) => {
   const ch_id = c.req.query("ch_id");
+  const ch_seq = c.req.query("ch_seq");
+  const ch_seq_url = c.req.query("ch_seq_url");
   if (ch_id) {
+    if (ch_seq) {
+      try {
+        let { results } = await c.env.DB.prepare(
+          `SELECT * FROM videos WHERE ch_id = '${ch_id}' AND ch_seq = '${ch_seq}'`
+        ).all();
+        return c.json({ result: results });
+      } catch (e) {
+        return c.json({ error: e }, 500);
+      }
+    }
     try {
       let { results } = await c.env.DB.prepare(
         `SELECT * FROM videos WHERE ch_id = '${ch_id}'`
@@ -16,6 +28,16 @@ chlistGet.get("/", async (c) => {
       return c.json({ result: results });
     } catch (e) {
       return c.json({ error: e }, 500);
+    }
+  }
+  if (ch_seq_url) {
+    try {
+      let { results } = await c.env.DB.prepare(
+        `SELECT * FROM videos WHERE ch_seq_url = '${ch_seq_url}'`
+      ).all();
+      return c.json({ result: results });
+    } catch (e) {
+      return c.json({ error: String(e) }, 500);
     }
   }
   try {
