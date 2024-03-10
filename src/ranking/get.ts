730dbe488e3dc9cb06ca6c98ce137fde5a035795
boxfish <boxfish893@gmail.com>
2024-03-10 21:12:31 +0900

    feat: rankingのorderを変えられるように

--- src/ranking/get.ts
+++ src/ranking/get.ts
@@ -14,12 +14,13 @@ rankingGet.get("/", async (c) => {
   const sseason = c.req.query("sseason");
   const limit = c.req.query("limit");
   const offset = c.req.query("offset");
+  const order = c.req.query("order");
   if (ch_id) {
     if (raddtime) {
       try {
-        let { results } = await c.env.DB.prepare(
-          `SELECT * FROM ranking WHERE ch_id = ${ch_id} AND raddtime < "${raddtime} ORDER BY daddtime DESC LIMIT 1"`
-        ).all();
+        const sql = `SELECT * FROM ranking WHERE ch_id = ${ch_id} AND raddtime < "${raddtime}" ORDER BY raddtime DESC LIMIT 1`;
+        console.log(sql);
+        let { results } = await c.env.DB.prepare(sql).all();
         return c.json({ result: results });
       } catch (e) {
         return c.json({ error: String(e) }, 500);
@@ -37,20 +38,20 @@ rankingGet.get("/", async (c) => {
   }
   if (syear && sseason && raddtime) {
     const between = betweenDay(new Date(raddtime));
-    if (limit && offset) {
+    if (limit && offset && order) {
       try {
-        let { results } = await c.env.DB.prepare(
-          `SELECT chlist.syear, chlist.sseason, ranking.* FROM ranking INNER JOIN chlist ON ranking.ch_id = chlist.ch_id WHERE chlist.syear = ${syear} AND chlist.sseason = ${sseason} AND ranking.raddtime > '${between[0]}' AND ranking.raddtime < '${between[1]}' ORDER BY r_ave_view_rank DESC LIMIT ${limit} OFFSET ${offset}`
-        ).all();
+        const sql = `SELECT chlist.syear, chlist.sseason, chlist.ch_title, chlist.ch_url, chlist.ch_LtstFree, chlist.ch_PrmFree, chlist.ch_thumb ,ranking.* FROM ranking INNER JOIN chlist ON ranking.ch_id = chlist.ch_id WHERE chlist.syear = ${syear} AND chlist.sseason = ${sseason} AND ranking.raddtime > '${between[0]}' AND ranking.raddtime < '${between[1]}' ORDER BY ${order} ASC LIMIT ${limit} OFFSET ${offset}`;
+        console.log(sql);
+        let { results } = await c.env.DB.prepare(sql).all();
         return c.json({ result: results });
       } catch (e) {
         return c.json({ error: String(e) }, 500);
       }
     }
     try {
-      let { results } = await c.env.DB.prepare(
-        `SELECT chlist.syear, chlist.sseason, ranking.* FROM ranking INNER JOIN chlist ON ranking.ch_id = chlist.ch_id WHERE chlist.syear = ${syear} AND chlist.sseason = ${sseason} AND ranking.raddtime > '${between[0]}' AND ranking.raddtime < '${between[1]}'`
-      ).all();
+      const sql = `SELECT chlist.syear, chlist.sseason, ranking.* FROM ranking INNER JOIN chlist ON ranking.ch_id = chlist.ch_id WHERE chlist.syear = ${syear} AND chlist.sseason = ${sseason} AND ranking.raddtime > '${between[0]}' AND ranking.raddtime < '${between[1]}'`;
+      console.log(sql);
+      let { results } = await c.env.DB.prepare(sql).all();
       return c.json({ result: results });
     } catch (e) {
       return c.json({ error: String(e) }, 500);
