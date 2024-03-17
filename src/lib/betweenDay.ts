615674092509e561b12256e6169f8266179cdf2e
boxfish <boxfish893@gmail.com>
2024-03-17 17:43:02 +0900

    feat: betweenDayのクエリを緩和させた

--- src/lib/betweenDay.ts
+++ src/lib/betweenDay.ts
@@ -1,5 +1,5 @@
 const betweenDay = (date: Date) => {
-  let beforeDay = new Date(date.getTime() - 24 * 60 * 60 * 1000);
+  let beforeDay = new Date(date.getTime() - 30 * 60 * 60 * 1000);
   return [
     beforeDay.toLocaleString("sv-SE", { timeZone: "UTC" }),
     date.toLocaleString("sv-SE", { timeZone: "UTC" }),
