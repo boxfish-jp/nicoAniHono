2184fcbe173789b92de043ba810b653fc877d51f
boxfish <boxfish893@gmail.com>
2024-03-17 17:38:37 +0900

    fix: DBとタイムゾーンが違うことにより、クエリできない問題を修正

--- src/lib/betweenDay.ts
+++ src/lib/betweenDay.ts
@@ -1,8 +1,8 @@
 const betweenDay = (date: Date) => {
   let beforeDay = new Date(date.getTime() - 24 * 60 * 60 * 1000);
   return [
-    beforeDay.toLocaleString("sv-SE", { timeZone: "Asia/Tokyo" }),
-    date.toLocaleString("sv-SE", { timeZone: "Asia/Tokyo" }),
+    beforeDay.toLocaleString("sv-SE", { timeZone: "UTC" }),
+    date.toLocaleString("sv-SE", { timeZone: "UTC" }),
   ];
 };
 
