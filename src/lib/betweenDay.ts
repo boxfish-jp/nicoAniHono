67f62888c3a456577016f6080fe63005ad32bf27
boxfish <fuguo892@gmail.com>
2024-04-01 22:47:29 +0900

    feat: looseと逆のstrictを追加

--- src/lib/betweenDay.ts
+++ src/lib/betweenDay.ts
@@ -1,8 +1,16 @@
-const betweenDay = (date: Date, loose: any) => {
-  const beforeDay =
-    loose != undefined
-      ? new Date(date.getTime() - 30 * 60 * 60 * 1000)
-      : new Date(date.getTime() - 24 * 60 * 60 * 1000);
+const betweenDay = (date: Date, loose: string | undefined) => {
+  let beforeDay : Date;
+  switch (loose) {
+    case undefined:
+      beforeDay = new Date(date.getTime() - 24 * 60 * 60 * 1000)
+      break;
+    case "strict":
+      beforeDay = new Date(date.getTime() - 23 * 60 * 60 * 1000);
+      break;
+    default:
+      beforeDay = new Date(date.getTime() - 30 * 60 * 60 * 1000);
+      break;
+  }
   return [
     beforeDay.toLocaleString("sv-SE", { timeZone: "UTC" }),
     date.toLocaleString("sv-SE", { timeZone: "UTC" }),
